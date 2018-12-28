f8a540ccc708290ba55fad32b7c88654e5483715
Efrat Traube <efratraube@gmail.com>
2018-12-28 03:08:57 -0800

    CLI header

--- CLI.h
+++ CLI.h
@@ -5,4 +5,33 @@
 #ifndef EXCELLENTEAM_ELLA_C_DNA_WALL_ET_CLI_H
 #define EXCELLENTEAM_ELLA_C_DNA_WALL_ET_CLI_H
 
+#include <map>
+#include "Parser.h"
+
+typedef void (*CommandFunction)(int* argc,char * argv[]); // function pointer type
+typedef std::map<char *, CommandFunction> commandsCallbackMap;
+
+class CLI
+{
+public:
+    static CLI* getInstance();
+    void run();
+
+private:
+    CLI();
+    ~CLI();
+    CLI(CLI const&); //not implemented
+    CLI& operator=(CLI const&); //not implemented
+
+    inline void readLine();
+    inline bool parseCommand();
+    inline void callFunctionByCommand() const;
+
+    static commandsCallbackMap m_commandsAPI;
+    char fullCommand[1000];
+    int* m_argc;
+    char * m_argv[10];
+
+};
+
 #endif //EXCELLENTEAM_ELLA_C_DNA_WALL_ET_CLI_H
