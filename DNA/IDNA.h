dfe9c6dd7ff3a0da0683fa603c11ca8aedfd6753
Efrat Traube <efratraube@gmail.com>
2018-12-29 12:44:42 -0800

    idna header

--- DNA/IDNA.h
+++ DNA/IDNA.h
@@ -7,14 +7,34 @@
 
 #include <iostream>
 
+#include "../SharedPtr.h"
+
 class IDNA
 {
 public:
     IDNA();
     virtual ~IDNA();
-    virtual size_t getDNALength() const = 0 ;
-    virtual char* operator[](const size_t index) = 0;
 private:
+protected:
+    virtual size_t getDNALength() const = 0 ;
+    virtual char  operator[](const size_t index) = 0;
+    class Nucleotide
+    {
+    protected:
+        Nucleotide(char c ='A');
+
+        bool operator == (char);
+        Nucleotide* operator = (char);
+        Nucleotide* operator = (Nucleotide);
+        operator char()
+        {
+            return m_singleNucleotide;
+        };
+
+        char m_singleNucleotide;
+    private:
+        void check_if_valid(char c_to_check);
+    };
 };
 
 #endif //EXCELLENTEAM_ELLA_C_DNA_WALL_ET_IDNA_H
